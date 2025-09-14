dac917d7f967242351e30f982c842ac2506448a8
TraumDE <eugene301workmaik@gmail.com>
2025-09-14 18:38:53 +0300

    Add logo icon

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
+<script lang="ts" setup>
+import AppLogo from './components/icons/AppLogo.vue'
+</script>
+
 <template>
+  <app-logo />
   <h1>You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <div class="cont"></div>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.cont {
+  width: 100px;
+  height: 100px;
+  background-color: aqua;
+
+  @media (--phone-small) {
+    height: 50px;
+  }
+}
+</style>
