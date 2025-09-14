f116b49a05e33e4c5b2c0f3841ffdc1c51d2a4b8
TraumDE <eugene301workmaik@gmail.com>
2025-09-14 20:03:44 +0300

    Add icons for nav

---
+++ src/components/icons/DropdownIcon.vue
@@ -0,0 +1,80 @@
+<template>
+  <svg width="11" height="6" viewBox="0 0 11 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g id="Border">
+      <mask id="path-1-inside-1_16_71" fill="white">
+        <path d="M0.72998 0.699951H10.73V5.69995H0.72998V0.699951Z" />
+      </mask>
+      <g
+        clip-path="url(#paint0_diamond_16_71_clip_path)"
+        data-figma-skip-parse="true"
+        mask="url(#path-1-inside-1_16_71)"
+      >
+        <g transform="matrix(0.005 0 0 0.005 5.72998 0.699951)">
+          <rect
+            x="0"
+            y="0"
+            width="2000"
+            height="2000"
+            fill="url(#paint0_diamond_16_71)"
+            opacity="1"
+            shape-rendering="crispEdges"
+          />
+          <rect
+            x="0"
+            y="0"
+            width="2000"
+            height="2000"
+            transform="scale(1 -1)"
+            fill="url(#paint0_diamond_16_71)"
+            opacity="1"
+            shape-rendering="crispEdges"
+          />
+          <rect
+            x="0"
+            y="0"
+            width="2000"
+            height="2000"
+            transform="scale(-1 1)"
+            fill="url(#paint0_diamond_16_71)"
+            opacity="1"
+            shape-rendering="crispEdges"
+          />
+          <rect
+            x="0"
+            y="0"
+            width="2000"
+            height="2000"
+            transform="scale(-1)"
+            fill="url(#paint0_diamond_16_71)"
+            opacity="1"
+            shape-rendering="crispEdges"
+          />
+        </g>
+      </g>
+      <path
+        d="M0.72998 0.699951V-4.30005H-4.27002V0.699951H0.72998ZM10.73 0.699951H15.73V-4.30005H10.73V0.699951ZM0.72998 5.69995H10.73V-4.30005H0.72998V5.69995ZM5.72998 0.699951V5.69995H15.73V0.699951H5.72998ZM5.72998 5.69995V0.699951H-4.27002V5.69995H5.72998Z"
+        data-figma-gradient-fill="{&#34;type&#34;:&#34;GRADIENT_DIAMOND&#34;,&#34;stops&#34;:[{&#34;color&#34;:{&#34;r&#34;:0.34509804844856262,&#34;g&#34;:0.33725491166114807,&#34;b&#34;:0.83921569585800171,&#34;a&#34;:1.0},&#34;position&#34;:0.99999988079071045},{&#34;color&#34;:{&#34;r&#34;:0.0,&#34;g&#34;:0.0,&#34;b&#34;:0.0,&#34;a&#34;:0.0},&#34;position&#34;:1.0}],&#34;stopsVar&#34;:[{&#34;color&#34;:{&#34;r&#34;:0.34509804844856262,&#34;g&#34;:0.33725491166114807,&#34;b&#34;:0.83921569585800171,&#34;a&#34;:1.0},&#34;position&#34;:0.99999988079071045},{&#34;color&#34;:{&#34;r&#34;:0.0,&#34;g&#34;:0.0,&#34;b&#34;:0.0,&#34;a&#34;:0.0},&#34;position&#34;:1.0}],&#34;transform&#34;:{&#34;m00&#34;:10.0,&#34;m01&#34;:0.0,&#34;m02&#34;:0.729980468750,&#34;m10&#34;:0.0,&#34;m11&#34;:10.0,&#34;m12&#34;:-4.3000488281250},&#34;opacity&#34;:1.0,&#34;blendMode&#34;:&#34;NORMAL&#34;,&#34;visible&#34;:true}"
+        mask="url(#path-1-inside-1_16_71)"
+      />
+    </g>
+    <defs>
+      <clipPath id="paint0_diamond_16_71_clip_path">
+        <path
+          d="M0.72998 0.699951V-4.30005H-4.27002V0.699951H0.72998ZM10.73 0.699951H15.73V-4.30005H10.73V0.699951ZM0.72998 5.69995H10.73V-4.30005H0.72998V5.69995ZM5.72998 0.699951V5.69995H15.73V0.699951H5.72998ZM5.72998 5.69995V0.699951H-4.27002V5.69995H5.72998Z"
+          mask="url(#path-1-inside-1_16_71)"
+        />
+      </clipPath>
+      <linearGradient
+        id="paint0_diamond_16_71"
+        x1="0"
+        y1="0"
+        x2="500"
+        y2="500"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="1" stop-color="#5856D6" />
+        <stop offset="1" stop-opacity="0" />
+      </linearGradient>
+    </defs>
+  </svg>
+</template>
